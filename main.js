854e065293cf059d41d09c0cafdbe3f5a5556f1d
murad_0799 <murodbest19@gmail.com>
2022-03-31 22:10:18 +0500

    first lesson end

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const app = Vue.createApp({
     // dinamik ma`lumotlar
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            description: 'A pair of warm, fuzzy socks'
         }
     },
 })
 
+//Elementga ulanish
 app.mount('#app')
