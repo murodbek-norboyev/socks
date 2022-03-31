a184953d38a72b5151c79aae1ad9a034927a19f2
murad_0799 <murodbest19@gmail.com>
2022-03-31 23:51:52 +0500

    thirty lesson end

--- conditionalRendering/main.js
+++ conditionalRendering/main.js
@@ -5,7 +5,11 @@ const app = Vue.createApp({
             product: 'Socks',
             image: '/assets/images/vmSocks-green-onWhite.jpg',
             altText: 'A pair of socks',
-            productLink: '/product'
+            productLink: '/product',
+            // yangi ma`lumot xususiyati
+            inStock: true,
+            onSale: true,
+            inventory: 100
         }
     },
 })
