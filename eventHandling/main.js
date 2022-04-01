b74706cb1d978b469468a6c9912e42489db1a0bf
murad_0799 <murodbest19@gmail.com>
2022-04-01 14:25:14 +0500

    5 lesson end

--- eventHandling/main.js
+++ eventHandling/main.js
@@ -5,31 +5,39 @@ const app = Vue.createApp({
             product: 'Socks',
             image: '/assets/images/vmSocks-green-onWhite.jpg',
             altText: 'A pair of socks',
-            productLink: '/product',
-            // yangi ma`lumot xususiyati
+            price: 150,
+            total: 0,
             inStock: true,
-            onSale: true,
-            //massiv
             details: [
                 '80% cotton',
                 '20% polyester',
                 'Gender-neutral'
             ],
-            sizes: [
-                'S',
-                'M',
-                'XL',
-                'XXL'
-            ],
             variants: [{
                     variantId: 2234,
-                    variantColor: 'green'
+                    variantColor: 'green',
+                    variantImage: '/assets/images/vmSocks-green-onWhite.jpg'
                 },
                 {
                     variantId: 2235,
-                    variantColor: 'blue'
+                    variantColor: 'blue',
+                    variantImage: '/assets/images/vmSocks-blue-onWhite.jpg'
                 }
-            ]
+            ],
+            cart: 0
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+            this.total = this.cart * this.price
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+        decrement(){
+            this.cart != 0 ? this.cart -= 1 : NaN
+            this.total != 0 ? this.total -= 150 : NaN
         }
     },
 })
