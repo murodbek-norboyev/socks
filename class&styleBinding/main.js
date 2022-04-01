937f54ce86d9e44ceac43977c49acc0a5275b046
murad_0799 <murodbest19@gmail.com>
2022-04-01 16:53:48 +0500

    6 lesson

---
+++ class&styleBinding/main.js
@@ -0,0 +1,40 @@
+const app = Vue.createApp({
+    // dinamik ma`lumotlar
+    data() {
+        return {
+            product: 'Socks',
+            image: '/assets/images/vmSocks-green-onWhite.jpg',
+            altText: 'A pair of socks',
+            inStock: false,
+            details: [
+                '80% cotton',
+                '20% polyester',
+                'Gender-neutral'
+            ],
+            variants: [{
+                    variantId: 2234,
+                    variantColor: 'green',
+                    variantImage: '/assets/images/vmSocks-green-onWhite.jpg'
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: '/assets/images/vmSocks-blue-onWhite.jpg'
+                }
+            ],
+            cart: 0
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+            this.total = this.cart * this.price
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+    },
+})
+
+//Elementga ulanish
+app.mount('#app')
